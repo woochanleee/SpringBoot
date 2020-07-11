f039ed1e99e29c00d59d227cd22fe53f0dc79a00
_uchanlee <uchanlee.dev@gmail.com>
2020-07-11 16:23:39 +0900

    [INIT] <board-crud> project setting

---
+++ board-crud/src/main/java/com/woochanleee/boardcrud/BoardCrudApplication.java
@@ -0,0 +1,13 @@
+package com.woochanleee.boardcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardCrudApplication.class, args);
+	}
+
+}
