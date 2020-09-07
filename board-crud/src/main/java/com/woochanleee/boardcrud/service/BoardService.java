f02e88e565d1f129b34363668e39019b6b55695e
_uchanlee <uchanlee.dev@gmail.com>
2020-09-07 18:52:57 +0900

    [ADD] <board-crud> BoardService.java
    getBoardList

--- board-crud/src/main/java/com/woochanleee/boardcrud/service/BoardService.java
+++ board-crud/src/main/java/com/woochanleee/boardcrud/service/BoardService.java
@@ -1,16 +1,42 @@
 package com.woochanleee.boardcrud.service;
 
+import com.woochanleee.boardcrud.domain.entity.BoardEntity;
 import com.woochanleee.boardcrud.dto.BoardDto;
 import com.woochanleee.boardcrud.domain.repository.BoardRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class BoardService {
     private BoardRepository boardRepository;
+
+    @Transactional
+    public List<BoardDto> getBoardList() {
+        List<BoardEntity> boardEntities = boardRepository.findAll();
+        List<BoardDto> boardDtoList= new ArrayList<>();
+
+        for (BoardEntity boardEntity : boardEntities) {
+            BoardDto boardDTO = BoardDto.builder()
+                    .id(boardEntity.getId())
+                    .title(boardEntity.getTitle())
+                    .content(boardEntity.getContent())
+                    .writer(boardEntity.getWriter())
+                    .createdDate(boardEntity.getCreatedDate())
+                    .build();
+
+            boardDtoList.add(boardDTO);
+        }
+
+        return boardDtoList;
+        /*
+            Controller와 Service간에 데이터 전달은 dto 객체로 하기 위해, Repository에서 가져온 Entity를 반복문을 통해 dto로 변환하는 작업이 있다.
+        */
+    }
 
     @Transactional
     public Long savePost(BoardDto boardDto) {
