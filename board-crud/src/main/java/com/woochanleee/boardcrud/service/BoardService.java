99798f39445c5e402d9a326aa99c6ba582e5b3b7
_uchanlee <uchanlee.dev@gmail.com>
2020-09-13 12:57:13 +0900

    [UPDATE] <board-crud> BoardService.java

--- board-crud/src/main/java/com/woochanleee/boardcrud/service/BoardService.java
+++ board-crud/src/main/java/com/woochanleee/boardcrud/service/BoardService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -39,8 +40,29 @@ public class BoardService {
     }
 
     @Transactional
+    public BoardDto getPost(Long id) {
+        Optional<BoardEntity> boardEntityWrapper = boardRepository.findById(id);
+        BoardEntity boardEntity = boardEntityWrapper.get();
+
+        BoardDto boardDto = BoardDto.builder()
+                .id(boardEntity.getId())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getContent())
+                .writer(boardEntity.getWriter())
+                .createdDate((boardEntity.getCreatedDate()))
+                .build();
+
+        return boardDto;
+    }
+
+    @Transactional
     public Long savePost(BoardDto boardDto) {
         return boardRepository.save(boardDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public void deletePost(Long id) {
+        boardRepository.deleteById(id);
     }
 }
 
