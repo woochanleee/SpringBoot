2d87639e0aa8433c4d7e7d2deb5930b6723f89db
_uchanlee <uchanlee.dev@gmail.com>
2020-10-29 11:51:15 +0900

    [ADD] <hello-spring> hello-mvc mapping

--- hello-spring/src/main/java/com/woochanleee/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/com/woochanleee/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.woochanleee.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,5 +13,12 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
 
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "hello-template";
     }
 }
