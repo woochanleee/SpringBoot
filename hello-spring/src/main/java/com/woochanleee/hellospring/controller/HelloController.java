aafee560c747f293abd4150badfd1e5c0c08b35d
_uchanlee <uchanlee.dev@gmail.com>
2020-10-29 14:13:57 +0900

    [ADD] <hello-spring> HelloController content-type json

--- hello-spring/src/main/java/com/woochanleee/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/com/woochanleee/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,5 +21,33 @@ public class HelloController {
         model.addAttribute("name", name);
 
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(value = "name") String name) {
+        return "hello " + name; // "hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(value = "name") String name) {
+        Hello hello = new Hello();
+
+        hello.setName(name);
+
+        return hello;
+    }
+
+    public static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
