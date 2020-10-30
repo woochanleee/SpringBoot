c45a366f8375e0e52295a53b3cc13e9f9fbfb554
_uchanlee <uchanlee.dev@gmail.com>
2020-10-30 20:54:51 +0900

    [ADD] <hello-spring> annotation component scan

--- hello-spring/src/main/java/com/woochanleee/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/com/woochanleee/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package com.woochanleee.hellospring.repository;
 
 import com.woochanleee.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
