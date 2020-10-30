414f89204540532188f35b4a9d03592c22d1a435
_uchanlee <uchanlee.dev@gmail.com>
2020-10-30 14:37:36 +0900

    [CREATE] <hello-spring> MemoryMemberRepository

---
+++ hello-spring/src/main/java/com/woochanleee/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package com.woochanleee.hellospring.repository;
+
+import com.woochanleee.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<Member>(store.values());
+    }
+
+    public void clear() {
+        store.clear();
+    }
+}
