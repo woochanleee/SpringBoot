37a09fe977bbf9555440e162e483b7b603290755
_uchanlee <uchanlee.dev@gmail.com>
2020-10-30 20:55:07 +0900

    [ADD] <hello-spring> annotation component scan

--- hello-spring/src/main/java/com/woochanleee/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/com/woochanleee/hellospring/service/MemberService.java
@@ -3,15 +3,19 @@ package com.woochanleee.hellospring.service;
 import com.woochanleee.hellospring.domain.Member;
 import com.woochanleee.hellospring.repository.MemberRepository;
 import com.woochanleee.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
