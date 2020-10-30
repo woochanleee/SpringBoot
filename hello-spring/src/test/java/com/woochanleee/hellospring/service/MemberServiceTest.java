254d7eb0f3058b5d3d785db595df0b9f9f31defe
_uchanlee <uchanlee.dev@gmail.com>
2020-10-30 19:31:54 +0900

    [CREATE] <hello-spring> MemberServiceTest

---
+++ hello-spring/src/test/java/com/woochanleee/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,81 @@
+package com.woochanleee.hellospring.service;
+
+import com.woochanleee.hellospring.domain.Member;
+import com.woochanleee.hellospring.repository.MemberRepository;
+import com.woochanleee.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clear();
+    }
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(member.getId()).get();
+
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+
+        member1.setName("spring");
+
+        Member member2  = new Member();
+
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+
+        String errorMessage = assertThrows(IllegalStateException.class, () -> memberService.join(member2)).getMessage();
+
+        assertThat(errorMessage).isEqualTo("이미 존재하는 회원입니다.");
+
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalStateException error) {
+//            assertThat(error.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+
+        // then
+    }
+
+   @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
