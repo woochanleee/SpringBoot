509e00c94760b3ff904b6bd907dad85ee78c3689
_uchanlee <uchanlee.dev@gmail.com>
2020-10-30 21:18:45 +0900

    [UPDATE] <hello-spring> Configuration bean

--- hello-spring/src/main/java/com/woochanleee/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/com/woochanleee/hellospring/service/MemberService.java
@@ -10,7 +10,6 @@ import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
