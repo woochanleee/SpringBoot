a9c73b5c5cb2d94ca934ac241ead8eada0436776
_uchanlee <uchanlee.dev@gmail.com>
2020-10-30 19:31:39 +0900

    [CREATE] <hello-spring> MemberService

---
+++ hello-spring/src/main/java/com/woochanleee/hellospring/service/MemberService.java
@@ -0,0 +1,47 @@
+package com.woochanleee.hellospring.service;
+
+import com.woochanleee.hellospring.domain.Member;
+import com.woochanleee.hellospring.repository.MemberRepository;
+import com.woochanleee.hellospring.repository.MemoryMemberRepository;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 x
+        validateDuplicateMember(member); // 중복 회원 검
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
