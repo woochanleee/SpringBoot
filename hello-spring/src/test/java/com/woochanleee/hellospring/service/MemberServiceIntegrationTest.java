97335d41e2475a25ae3367a962cb3728528e2884
_uchanlee <uchanlee.dev@gmail.com>
2020-11-01 01:38:49 +0900

    [ADD] <hello-spring> Commit test case

--- hello-spring/src/test/java/com/woochanleee/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/com/woochanleee/hellospring/service/MemberServiceIntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,12 +23,12 @@ class MemberServiceIntegrationTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
+    @Commit
     void 회원가입() {
         // given
         Member member = new Member();
 
-        member.setName("hello2" +
-                "");
+        member.setName("hello2");
 
         // when
         Long saveId = memberService.join(member);
