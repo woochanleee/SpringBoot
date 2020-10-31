3c956334d71aa858edbf81915e372b0d3897cd99
_uchanlee <uchanlee.dev@gmail.com>
2020-11-01 01:38:27 +0900

    [UPDATE] <hello-spring> Configuration

--- hello-spring/src/main/java/com/woochanleee/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/com/woochanleee/hellospring/SpringConfig.java
@@ -1,25 +1,32 @@
 package com.woochanleee.hellospring;
 
-import com.woochanleee.hellospring.repository.JdbcMemberRepository;
-import com.woochanleee.hellospring.repository.JdbcTemplateMemberRepository;
-import com.woochanleee.hellospring.repository.MemberRepository;
-import com.woochanleee.hellospring.repository.MemoryMemberRepository;
+import com.woochanleee.hellospring.repository.*;
 import com.woochanleee.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
+//    @PersistenceContext
+    private final EntityManager entityManager;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
+
+    //    private final DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
 
     @Bean
@@ -31,6 +38,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(entityManager);
     }
 }
