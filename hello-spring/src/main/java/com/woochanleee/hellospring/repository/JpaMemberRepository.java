4cd28852c8bace6076fc1e894352cb04a70b39bd
_uchanlee <uchanlee.dev@gmail.com>
2020-11-01 01:37:47 +0900

    [ADD] <hello-spring> JpaMemerRepository

---
+++ hello-spring/src/main/java/com/woochanleee/hellospring/repository/JpaMemberRepository.java
@@ -0,0 +1,45 @@
+package com.woochanleee.hellospring.repository;
+
+import com.woochanleee.hellospring.domain.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository {
+
+    private final EntityManager entityManager;
+
+    public JpaMemberRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Member save(Member member) {
+        entityManager.persist(member);
+
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = entityManager.find(Member.class, id);
+
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = entityManager.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return entityManager.createQuery("select m from Member as m", Member.class)
+                .getResultList();
+    }
+}
