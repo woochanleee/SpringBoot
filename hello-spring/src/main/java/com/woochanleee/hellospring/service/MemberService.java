f08e721fe1f55e47822978b6f2b6d559e2e60f63
_uchanlee <uchanlee.dev@gmail.com>
2020-11-01 02:19:50 +0900

    [ADD] <hello-spring> time trace

--- hello-spring/src/main/java/com/woochanleee/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/com/woochanleee/hellospring/service/MemberService.java
@@ -26,11 +26,20 @@ public class MemberService {
      */
 //    @Transactional
     public Long join(Member member) {
+        long start = System.currentTimeMillis();
+
+        try {
         // 같은 이름이 있는 중복 회원 x
         validateDuplicateMember(member); // 중복 회원 검
         memberRepository.save(member);
 
-        return member.getId();
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -44,7 +53,16 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
