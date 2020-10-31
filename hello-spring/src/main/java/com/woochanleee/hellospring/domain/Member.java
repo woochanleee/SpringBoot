6f02cf5626eba98ef511ec1719c5edb6630f3598
_uchanlee <uchanlee.dev@gmail.com>
2020-11-01 01:37:38 +0900

    [ADD] <hello-spring> Entity mapping

--- hello-spring/src/main/java/com/woochanleee/hellospring/domain/Member.java
+++ hello-spring/src/main/java/com/woochanleee/hellospring/domain/Member.java
@@ -1,7 +1,15 @@
 package com.woochanleee.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
 
     public Long getId() {
