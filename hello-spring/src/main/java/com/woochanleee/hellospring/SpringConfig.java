e7d869130018a310e23f7b075eb999f799ab111d
_uchanlee <uchanlee.dev@gmail.com>
2020-11-01 00:25:59 +0900

    [UPDATE] <hello-spring> Configuration

--- hello-spring/src/main/java/com/woochanleee/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/com/woochanleee/hellospring/SpringConfig.java
@@ -1,13 +1,25 @@
 package com.woochanleee.hellospring;
 
+import com.woochanleee.hellospring.repository.JdbcMemberRepository;
 import com.woochanleee.hellospring.repository.MemberRepository;
 import com.woochanleee.hellospring.repository.MemoryMemberRepository;
 import com.woochanleee.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
